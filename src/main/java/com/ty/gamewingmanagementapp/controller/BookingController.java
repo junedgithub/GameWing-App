d19c50fb5d270d8a62bc2c68ecc26826fe98c4e5
Chakoleradhika <chakoleradhika@gmail.com>
2024-01-09 18:25:36 +0530

    swagger documentation added

--- src/main/java/com/ty/gamewingmanagementapp/controller/BookingController.java
+++ src/main/java/com/ty/gamewingmanagementapp/controller/BookingController.java
@@ -12,19 +12,26 @@ import com.ty.gamewingmanagementapp.dto.Booking;
 import com.ty.gamewingmanagementapp.dto.ResponseStructure;
 import com.ty.gamewingmanagementapp.service.BookingService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class BookingController 
 {
 	@Autowired
 	private BookingService bookingService;
 	
+	@Operation(description = "Make Booking",summary = "Booking got created ")
+    @ApiResponses(value = {@ApiResponse(description = "Booking created",responseCode = "302"),@ApiResponse(description = "Booking Failed",responseCode = "404")})
 	 @PostMapping("/booking/slotId/{slotId}/customerId/{customerId}")
 	    public ResponseEntity<ResponseStructure<Booking>> booking(@RequestBody  Booking booking ,@PathVariable int slotId ,@PathVariable int customerId)
 	    {
 	    	return bookingService.booking(booking,slotId,customerId);
 	    }
 	 
-	 
+	@Operation(description = "Cancel Booking",summary = "Booking got cancelled")
+    @ApiResponses(value = {@ApiResponse(description = "Booking Cancel",responseCode = "302"),@ApiResponse(description = "Not Deleted",responseCode = "404")})
 	 @DeleteMapping("/cancelBooking/BookingId/{bookingId}")
 	 public ResponseEntity<ResponseStructure<Booking>>  cancelBooking(@PathVariable int bookingId)
 	 {
